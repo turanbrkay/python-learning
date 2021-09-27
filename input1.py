bf36a9eac6862f424cfc6f04a11a50003b872496
turanbrkay <firminoloji9@gmail.com>
2021-09-27 20:46:41 +0300

    Create input1.py

---
+++ input1.py
@@ -0,0 +1,4 @@
+takım=input("Favori takımınız nedir?")
+tekrar=input("Takımınızı kaç kere tekrar edelim?")
+print(takım*int(tekrar))
+# Burada int fonksiyonunu yazmayı iyi düşündün kendi başına ilk hata bulman:)
