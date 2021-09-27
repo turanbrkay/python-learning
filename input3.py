fb8b2e1436f5dbfe2a3c7625470bfb0a65521ebb
turanbrkay <firminoloji9@gmail.com>
2021-09-27 20:46:46 +0300

    Create input3.py

---
+++ input3.py
@@ -0,0 +1,28 @@
+# aylık fatura hesaplama
+
+ocak = mart = mayıs = temmuz = ağustos = ekim = aralık = 31
+nisan = haziran = eylül = kasım = 30
+şubat = 28
+
+birimfiyat=1.14
+
+print("""
+/////////////////////////////////////////////////////////////////
+///   Aylık Doğalgaz Borcunuzu Öğrenme Sistemine Hoşgeldiniz  ///
+/////////////////////////////////////////////////////////////////
+""")
+
+
+hangiay=input("Lütfen hangi aya ait borcunuzu öğrenmek istediğinizi giriniz\nGireceğiniz ayı küçük harflerle yazınız...  ")
+print("*"*100)
+aylıktüketim=input("Lütfen ay içerisinde tükettiğiniz doğalgaz miktarını metreküp cinsinden giriniz...")
+
+ay=eval(hangiay)
+
+birimtüketim= int(aylıktüketim) / ay
+tüketimiktar= birimtüketim * ay * birimfiyat
+
+print("*"*100)
+print("Fatura miktarınız:",tüketimiktar)
+
+# burada patladığım nokta eval fonksiyonu.
