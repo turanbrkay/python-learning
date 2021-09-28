6a584ba7123a983b5002340fa217f802f48f98f7
turanbrkay <firminoloji9@gmail.com>
2021-09-28 22:13:26 +0300

    Create format1.py

---
+++ format1.py
@@ -0,0 +1,8 @@
+# format fonksiyonu süslü parantezler arasını doldurur
+
+
+url=input("Lütfen ziyaret etmek istediğiniz sitenin adresini giriniz...")
+print(" {}  sitesine ulaşılamadı. Lütfen kontrol ederek tekrar deneyiniz." .format(url))
+
+# Görüldüğü gibi kullanıcıdan aldığımız veriyi format ile yazdırıyoruz
+
