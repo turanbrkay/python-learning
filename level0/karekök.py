17f69a4cf89aaf02fab83a7f93bd325297a8c3fe
turanbrkay <firminoloji9@gmail.com>
2021-09-29 15:58:51 +0300

    Update karekök.py

--- level0/karekök.py
+++ level0/karekök.py
@@ -4,6 +4,8 @@ xsayisi=36
 print(math.sqrt(xsayisi))                  # (run:6.0)
 # önce math kütüphanesine import ediyoruz. Ardından ise math.sqrt(xsayisi) şeklinde istediğimiz sayının karekökünü alıyoruz
 
+print(xsayisi**0.5)    # bu şekilde de hesaplayabilirsin bir sayının 0.5 üssü karekökü demektir
+
 # üslü ifade için
 y=2
 print(y**5)
